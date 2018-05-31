639a838c8b722e0db1a54e28f491d016fc73273a
Valentin Janvier <alencruserxx@hotmail.fr>
2018-05-31 20:22:50 +0200

    ajout script qui affiche explication lors clic sur l'input mdp et retrait affichage a bout de 5sec

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 window.addEventListener('load',function(){
 	document.getElementById('res').reset()
 })
+document.getElementById('mdp').addEventListener('click',function(){
+	document.getElementById('necessaire').style.display='inline';
+	setTimeout(turnback,5000);
+})
+function turnback(){
+	document.getElementById('necessaire').style.display='none';
+}
