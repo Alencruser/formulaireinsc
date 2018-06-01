a7ed2ed8b17fa338e1b3548c0a439290e2b04721
Valentin Janvier <alencruserxx@hotmail.fr>
2018-06-01 16:46:12 +0200

    petits rajouts insignifiants

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ function stocker(){
     var store={"Firstname:": $('#firstname').val(),"lastname":$('#lastname').val(),"Pseudo":$('#pseudo').val(),"password":$('#pass').val(),"Sexe":lab,"Birthdate":$('#date').val(),"city":$('#city').val(),"mail":$('#email').val(),"website":$('#site').val(),"Hobby":$('#hobbies').val(),"phone":$('#tel').val(),"color":$('#color').val()};
 	localStorage.setItem($(':input')[2].value,JSON.stringify(store))
 }
+//faire la verif si un user existe dejà ici
 function recuperer(){
-	var stocked=localStorage.getItem('Alencruser')
+	var stocked=localStorage.getItem('Simon')
 	console.log(JSON.parse(stocked))
 }
 $('.disp').keyup(function(){
