4e67dd160416bb0218dfc7f133a98695dc046805
Valentin Janvier <alencruserxx@hotmail.fr>
2018-06-01 14:20:53 +0200

    debug localstorage sortie du mot de passe et script + lisible

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 window.addEventListener('load',function(){
 	document.getElementById('res').reset();
 });
-var store={[$('#firstname').attr('placeholder')]: [$('#firstname').val()],[$('#lastname').attr('placeholder')]:[$('#lastname').val()],[$('#mdp').attr('placeholder')]:[$('#mdp').val()]};
+var lab;
+function stocker(){
+	if($(':input')[5].checked){
+	lab="Homme"
+} else {
+	lab='Femme'
+}
+    var store={"Nom:": $('#firstname').val(),"Prenom":$('#lastname').val(),"Pseudo":$('#pseudo').val(),"Motdepasse":$('#pass').val(),"Sexe":lab};
+	localStorage.setItem($(':input')[2].value,JSON.stringify(store))
+}
+function recuperer(){
+	var stocked=localStorage.getItem('Alencruser')
+	console.log(JSON.parse(stocked))
+}
 $('.disp').keyup(function(){
 	var x = $('#'+$(this).attr('data'))
-	$('#'+$(this).attr('data')).css('display','inline');
+	x.css('display','inline');
 	setTimeout(turnback,10000);
 	function turnback(){
 		x.css('display','none')
 	}
 })
-function stocker(){
-	localStorage.setItem($('#pseudo').val(),JSON.stringify(store))
-}
-	function recuperer(){
-	var stocked=localStorage.getItem('Simon')
-	console.log(JSON.parse(stocked))
-}
