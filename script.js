7f9896d168c46aa0a7502e9a67acaf78b8d8245a
Valentin Janvier <alencruserxx@hotmail.fr>
2018-06-01 10:34:34 +0200

    passage de js natif a jquery pour gagner du temps et de l'espace

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 window.addEventListener('load',function(){
-	document.getElementById('res').reset()
+	document.getElementById('res').reset();
+});
+$('.disp').keyup(function(){
+	var x = $('#'+$(this).attr('data'))
+	$('#'+$(this).attr('data')).css('display','inline');
+	setTimeout(turnback,10000);
+	function turnback(){
+		x.css('display','none')
+	}
 })
-document.getElementById('mdp').addEventListener('click',function(){
-	document.getElementById('necessaire').style.display='inline';
-	setTimeout(turnback,5000);
-})
-function turnback(){
-	document.getElementById('necessaire').style.display='none';
-}
