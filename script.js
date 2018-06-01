a7994d4ca39bab520b936a1e8d7704014879f5e2
Valentin Janvier <alencruserxx@hotmail.fr>
2018-06-01 12:47:03 +0200

    ajout du local storage en petit echantillon

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.addEventListener('load',function(){
 	document.getElementById('res').reset();
 });
+var store={[$('#firstname').attr('placeholder')]: [$('#firstname').val()],[$('#lastname').attr('placeholder')]:[$('#lastname').val()],[$('#mdp').attr('placeholder')]:[$('#mdp').val()]};
 $('.disp').keyup(function(){
 	var x = $('#'+$(this).attr('data'))
 	$('#'+$(this).attr('data')).css('display','inline');
@@ -9,3 +10,10 @@ $('.disp').keyup(function(){
 		x.css('display','none')
 	}
 })
+function stocker(){
+	localStorage.setItem($('#pseudo').val(),JSON.stringify(store))
+}
+	function recuperer(){
+	var stocked=localStorage.getItem('Simon')
+	console.log(JSON.parse(stocked))
+}
